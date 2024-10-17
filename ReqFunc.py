c83ac33f5986ccf26ad53f7aaaa304a78cce7145
CansizB <138613983+CansizB@users.noreply.github.com>
2024-10-17 11:37:42 +0300

    Update ReqFunc.py

--- ReqFunc.py
+++ ReqFunc.py
@@ -1,3 +1,110 @@
+class ImgRecon:
+    def __init__(self, path):
+        self.path = path
+  
+    def get_slice(self):
+        a=pd.read_csv(self.path,header=None)
+        i=np.array(a)
+        i[np.isnan(i)] = 0
+        i[i==0] = np.nan
+        min_val = np.nanmin(i)
+        max_val = np.nanmax(i)
+        normalized_data = (i - min_val) / (max_val - min_val) * 255
+    
+    
+        img_size = 32
+        nb_img_vert = normalized_data.shape[0] // img_size
+    
+        imgs = []
+    
+        for j in range(nb_img_vert):
+            
+            img = normalized_data[j * img_size: (j + 1) * img_size]
+            img[np.isnan(img)] = 0
+            imgs.append(img)
+    
+        slice = np.stack(imgs, axis=0)
+        return slice
+    
+    def RoiFrame(self, slice, start, end):
+   
+        frames=[]
+        for i in range(start,end):
+            frames.append(slice[i])
+    
+        frames=np.array(frames)
+    
+        return frames
+
+
+    def KFOLD(self):
+
+    def CSVtoNumPy(self, class_info: str, meta, file_name, CSVname)
+      train = []
+      test = []
+      
+      for i in range(0, 78):
+          subject = i + 1
+      
+          if meta["Diagnosis"][i] == class_info:
+      
+              for j in range(len(file_name)):
+      
+                  if file_name["SubjectID"][j] == subject:
+      
+                      if file_name["SubjectID"][j] in train:
+                          path = file_name["EITFilename"][j]
+      
+      
+                          slice = get_slice(CSVname + "/" + path[:-4] + ".csv")
+                          start = round(len(slice) / 2)-165
+                          end = round(len(slice) / 2)+165
+      
+                          frames = RoiFrame(slice, start, end)
+      
+                          train.append(frames)
+      
+                      if file_name["SubjectID"][j] in test:
+                          path = file_name["EITFilename"][j]
+      
+      
+                          slice = get_slice(CSVname + "/" + path[:-4] + ".csv")
+                          start = round(len(slice) / 2)-165
+                          end = round(len(slice) / 2)+165
+      
+                          frames = RoiFrame(slice, start, end)
+      
+                          test.append(frames)
+      
+      train = np.array(train)
+      test = np.array(test)
+      return train, test
+      
+    def GenSet(self, path: str):
+      train=train_idx_5
+      test=test_idx_5
+
+      CSV_name= path
+
+      COPD_train, COPD_test = CSVtoNumPy("COPD")
+      ILD_train, ILD_test = CSVtoNumPy("ILD")
+      Healthy_train, Healthy_test = CSVtoNumPy("Healthy")
+      Asthma_train, Asthma_test = CSVtoNumPy("Asthma")
+      Infection_train, Infection_test = CSVtoNumPy("Infection")
+
+      X_train=np.concatenate([COPD_train,ILD_train,Asthma_train,Infection_train,Healthy_train])
+      X_test=np.concatenate([COPD_test,ILD_test,Asthma_test,Infection_test,Healthy_test])
+
+      y_train= np.concatenate([np.ones(COPD_train.shape[0]),3*np.ones(ILD_train.shape[0]),np.zeros(Asthma_train.shape[0]),4*np.ones(Infection_train.shape[0]),2*np.ones(Healthy_train.shape[0])])
+      y_test= np.concatenate([np.ones(COPD_test.shape[0]),3*np.ones(ILD_test.shape[0]),np.zeros(Asthma_test.shape[0]),4*np.ones(Infection_test.shape[0]),2*np.ones(Healthy_test.shape[0])])
+
+      return X_train, X_test, y_train, y_test
+      
+      
+      
+
+
+
 def ftExp(ExpName: str, nb_epoch: int, nb_classes: int):
   
   for i in range(5):
