618bbf022e6b700e594d9fb716f44d7e8bba2c38
CansizB <138613983+CansizB@users.noreply.github.com>
2024-10-18 12:53:10 +0300

    Update ReqFunc.py

--- ReqFunc.py
+++ ReqFunc.py
@@ -124,24 +124,23 @@ class ImgRecon:
 
 
 
-def ftExp(net, ExpName, nb_epoch, nb_classes):
-    for i in range(5):
-        fold = str(i + 1)
-        y_train = np.load(f"path/fold_{fold}/y_train.npy")
-        y_test = np.load(f"path/fold_{fold}/y_test.npy")
-        X_train = np.load(f"path/fold_{fold}/X_train.npy")
-        X_test = np.load(f"path/fold_{fold}/X_test.npy")
+def ftExp(net, fold, ExpName, nb_epoch, nb_classes):
+    #for i in range(5):
+    fold = str(i + 1)
+    y_train = np.load(f"path/fold_{fold}/y_train.npy")
+    y_test = np.load(f"path/fold_{fold}/y_test.npy")
+    X_train = np.load(f"path/fold_{fold}/X_train.npy")
+    X_test = np.load(f"path/fold_{fold}/X_test.npy")
 
-        X_train3d = makeRGB(X_train, 330)
-        X_test3d = makeRGB(X_test, 330)
-        X_train3d, y_train = shuffle(X_train3d, np.array(y_train), random_state=42)
+    X_train3d = makeRGB(X_train, 330)
+    X_test3d = makeRGB(X_test, 330)
+    X_train3d, y_train = shuffle(X_train3d, np.array(y_train), random_state=42)
 
-        y_trainC = tf.keras.utils.to_categorical(y_train, num_classes=nb_classes)
-        y_testC = tf.keras.utils.to_categorical(y_test, num_classes=nb_classes)
+    y_trainC = tf.keras.utils.to_categorical(y_train, num_classes=nb_classes)
+    y_testC = tf.keras.utils.to_categorical(y_test, num_classes=nb_classes)
 
-        history = net.fit(X_train3d, y_trainC, epochs=nb_epoch, batch_size=64, validation_data=(X_test3d, y_testC))
-        net.save(ExpName + "Fold" + fold + ".keras")
-        del net
+    history = net.fit(X_train3d, y_trainC, epochs=nb_epoch, batch_size=64, validation_data=(X_test3d, y_testC))
+    net.save(ExpName + "Fold" + fold + ".keras")
 
 def extract_features(model, data):
     features = model.predict(data)
