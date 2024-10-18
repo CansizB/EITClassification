886b578d3b03a68f38489e7ae7ebf12a24c666a8
CansizB <138613983+CansizB@users.noreply.github.com>
2024-10-18 12:03:09 +0300

    Update ReqFunc.py

--- ReqFunc.py
+++ ReqFunc.py
@@ -1,147 +1,140 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from sklearn.utils import shuffle
+
 class ImgRecon:
     def __init__(self, path):
         self.path = path
-  
-    def get_slice(self):
-        a=pd.read_csv(self.path,header=None)
-        i=np.array(a)
-        i[np.isnan(i)] = 0
-        i[i==0] = np.nan
-        min_val = np.nanmin(i)
-        max_val = np.nanmax(i)
-        normalized_data = (i - min_val) / (max_val - min_val) * 255
-    
-    
+
+    def get_slice(self, CSVname):
+        # Read and process the CSV data
+        data = pd.read_csv(CSVname, header=None).values
+        data[np.isnan(data)] = 0  # Replace NaNs with 0
+        data[data == 0] = np.nan  # Convert 0s back to NaN
+        min_val, max_val = np.nanmin(data), np.nanmax(data)
+        normalized_data = (data - min_val) / (max_val - min_val) * 255
+
+        # Slice images into chunks
         img_size = 32
         nb_img_vert = normalized_data.shape[0] // img_size
-    
-        imgs = []
-    
-        for j in range(nb_img_vert):
-            
-            img = normalized_data[j * img_size: (j + 1) * img_size]
-            img[np.isnan(img)] = 0
-            imgs.append(img)
-    
-        slice = np.stack(imgs, axis=0)
-        return slice
-    
-    def RoiFrame(self, slice, start, end):
-   
-        frames=[]
-        for i in range(start,end):
-            frames.append(slice[i])
-    
-        frames=np.array(frames)
-    
-        return frames
+        imgs = [normalized_data[j * img_size: (j + 1) * img_size] for j in range(nb_img_vert)]
+        return np.stack(imgs, axis=0)
+
+    def RoiFrame(self, slice_data, start, end):
+        return np.array([slice_data[i] for i in range(start, end)])
+
+    def KFOLD(self, nb_subj, meta_path: str):
+
+        xph=np.random.random(nb_subj)
+        meta=pd.read_csv(meta_path + "/Metadata.txt")
+        meta.reset_index(drop=True, inplace=True)
+        yph=meta["Diagnosis"]
+        label_encoder = LabelEncoder()
+        yph = label_encoder.fit_transform(yph)
+        
+        # yph = np.where(yph == 0, 0, np.where(yph == 1, 0, np.where(yph == 2, 1, 2))) #if 3 class structure is implemented 
+        # yph = np.where(yph == 2, 1, 0) #if binary structure is implemented 
 
 
-    def KFOLD(self):
+        # Stratified K-Fold setup
+        k = 5
+        seed = 42
+        kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=seed)
+        all_train_idx, all_test_idx = [], []
 
-    def CSVtoNumPy(self, class_info: str, meta, file_name, CSVname)
-      train = []
-      test = []
-      
-      for i in range(0, 78):
-          subject = i + 1
-      
-          if meta["Diagnosis"][i] == class_info:
-      
-              for j in range(len(file_name)):
-      
-                  if file_name["SubjectID"][j] == subject:
-      
-                      if file_name["SubjectID"][j] in train:
-                          path = file_name["EITFilename"][j]
-      
-      
-                          slice = get_slice(CSVname + "/" + path[:-4] + ".csv")
-                          start = round(len(slice) / 2)-165
-                          end = round(len(slice) / 2)+165
-      
-                          frames = RoiFrame(slice, start, end)
-      
-                          train.append(frames)
-      
-                      if file_name["SubjectID"][j] in test:
-                          path = file_name["EITFilename"][j]
-      
-      
-                          slice = get_slice(CSVname + "/" + path[:-4] + ".csv")
-                          start = round(len(slice) / 2)-165
-                          end = round(len(slice) / 2)+165
-      
-                          frames = RoiFrame(slice, start, end)
-      
-                          test.append(frames)
-      
-      train = np.array(train)
-      test = np.array(test)
-      return train, test
-      
-    def GenSet(self, path: str):
-      train=train_idx_5
-      test=test_idx_5
+        for train_idx, test_idx in kf.split(xph, yph):
+            all_train_idx.append(train_idx)
+            all_test_idx.append(test_idx)
 
-      CSV_name= path
+        # Create separate train/test indices for 5 folds
+        self.train_test_splits = [(all_train_idx[i] + 1, all_test_idx[i] + 1) for i in range(5)]
 
-      COPD_train, COPD_test = CSVtoNumPy("COPD")
-      ILD_train, ILD_test = CSVtoNumPy("ILD")
-      Healthy_train, Healthy_test = CSVtoNumPy("Healthy")
-      Asthma_train, Asthma_test = CSVtoNumPy("Asthma")
-      Infection_train, Infection_test = CSVtoNumPy("Infection")
+    def CSVtoNumPy(self, class_info, meta, file_name, CSVname):
+        train, test = [], []
 
-      X_train=np.concatenate([COPD_train,ILD_train,Asthma_train,Infection_train,Healthy_train])
-      X_test=np.concatenate([COPD_test,ILD_test,Asthma_test,Infection_test,Healthy_test])
+        for i in range(0, 78):
+            subject = i + 1
+            if meta["Diagnosis"][i] == class_info:
+                for j in range(len(file_name)):
+                    if file_name["SubjectID"][j] == subject:
+                        path = file_name["EITFilename"][j]
+                        slice_data = self.get_slice(CSVname + "/" + path[:-4] + ".csv")
+                        start, end = round(len(slice_data) / 2) - 165, round(len(slice_data) / 2) + 165
+                        frames = self.RoiFrame(slice_data, start, end)
 
-      y_train= np.concatenate([np.ones(COPD_train.shape[0]),3*np.ones(ILD_train.shape[0]),np.zeros(Asthma_train.shape[0]),4*np.ones(Infection_train.shape[0]),2*np.ones(Healthy_train.shape[0])])
-      y_test= np.concatenate([np.ones(COPD_test.shape[0]),3*np.ones(ILD_test.shape[0]),np.zeros(Asthma_test.shape[0]),4*np.ones(Infection_test.shape[0]),2*np.ones(Healthy_test.shape[0])])
+                        if subject in train:
+                            train.append(frames)
+                        if subject in test:
+                            test.append(frames)
 
-      return X_train, X_test, y_train, y_test
-      
-      
-      
+        return np.array(train), np.array(test)
 
-
-
-def ftExp(ExpName: str, nb_epoch: int, nb_classes: int):
-  
-  for i in range(5):
-  
-    c= str(i+1)
+    def GenSet(self, fold, path, save=False):
+        # Assuming train/test indices are set from KFOLD
+        train_idx, test_idx = self.train_test_splits[fold]
+        CSV_name = path
     
-    y_train= np.load("path/fold_" + c + "/y_train.npy")
-    y_test= np.load("path/fold_" + c + "/y_test.npy")
+        # Generate datasets for different classes
+        COPD_train, COPD_test = self.CSVtoNumPy("COPD", meta, file_name, CSV_name)
+        ILD_train, ILD_test = self.CSVtoNumPy("ILD", meta, file_name, CSV_name)
+        Healthy_train, Healthy_test = self.CSVtoNumPy("Healthy", meta, file_name, CSV_name)
+        Asthma_train, Asthma_test = self.CSVtoNumPy("Asthma", meta, file_name, CSV_name)
+        Infection_train, Infection_test = self.CSVtoNumPy("Infection", meta, file_name, CSV_name)
     
-    X_train= np.load("path/fold_" + c + "/X_train.npy")
-    X_test= np.load("path/fold_" + c + "/X_test.npy")
+        # Concatenate and label the data
+        X_train = np.concatenate([COPD_train, ILD_train, Asthma_train, Infection_train, Healthy_train])
+        X_test = np.concatenate([COPD_test, ILD_test, Asthma_test, Infection_test, Healthy_test])
+        
+        y_train = np.concatenate([np.ones(COPD_train.shape[0]), 
+                                  3 * np.ones(ILD_train.shape[0]), 
+                                  np.zeros(Asthma_train.shape[0]), 
+                                  4 * np.ones(Infection_train.shape[0]), 
+                                  2 * np.ones(Healthy_train.shape[0])])
+        
+        y_test = np.concatenate([np.ones(COPD_test.shape[0]), 
+                                 3 * np.ones(ILD_test.shape[0]), 
+                                 np.zeros(Asthma_test.shape[0]), 
+                                 4 * np.ones(Infection_test.shape[0]), 
+                                 2 * np.ones(Healthy_test.shape[0])])
     
-    X_train3d= makeRGB(X_train, 330)
-    X_test3d= makeRGB(X_test,330)
+        # Optionally save the arrays to .npy files
+        if save:
+            np.save(f"{path}/fold_{fold}/X_train.npy", X_train)
+            np.save(f"{path}/fold_{fold}/X_test.npy", X_test)
+            np.save(f"{path}/fold_{fold}/y_train.npy", y_train)
+            np.save(f"{path}/fold_{fold}/y_test.npy", y_test)
     
-    X_train3d, y_train = shuffle(X_train3d, np.array(y_train), random_state=42)
-    
-    y_trainC = tf.keras.utils.to_categorical(y_train, num_classes=nb_classes)
-    y_testC = tf.keras.utils.to_categorical(y_test, num_classes=nb_classes)
-    
-    history = net.fit(X_train3d, y_trainC, epochs=nb_epoch, batch_size=64, validation_data=(X_test3d,y_testC))
-    
-    net.save(ExpName + "Fold" + c + ".keras")
-    
-    del net
+        return X_train, X_test, y_train, y_test
+
+
+
+def ftExp(ExpName, nb_epoch, nb_classes):
+    for i in range(5):
+        fold = str(i + 1)
+        y_train = np.load(f"path/fold_{fold}/y_train.npy")
+        y_test = np.load(f"path/fold_{fold}/y_test.npy")
+        X_train = np.load(f"path/fold_{fold}/X_train.npy")
+        X_test = np.load(f"path/fold_{fold}/X_test.npy")
+
+        X_train3d = makeRGB(X_train, 330)
+        X_test3d = makeRGB(X_test, 330)
+        X_train3d, y_train = shuffle(X_train3d, np.array(y_train), random_state=42)
+
+        y_trainC = tf.keras.utils.to_categorical(y_train, num_classes=nb_classes)
+        y_testC = tf.keras.utils.to_categorical(y_test, num_classes=nb_classes)
+
+        history = net.fit(X_train3d, y_trainC, epochs=nb_epoch, batch_size=64, validation_data=(X_test3d, y_testC))
+        net.save(ExpName + "Fold" + fold + ".keras")
+        del net
 
 
 def modelImp(ExpName, foldId):
-
-    Id = str(foldId)
-
-    Ftmodel = tf.keras.models.load_model(ExpName + "Fold" +Id+ ".keras")
-
-    pretrained_layers = Ftmodel.layers[:-1] 
-
+    fold_str = str(foldId)
+    Ftmodel = tf.keras.models.load_model(ExpName + "Fold" + fold_str + ".keras")
+    pretrained_layers = Ftmodel.layers[:-1]
     Ftmodel = Model(inputs=Ftmodel.input, outputs=pretrained_layers[-1].output)
-
     Ftmodel.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
     return Ftmodel
