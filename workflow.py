5d4fe03358d29d38dcdbd47b7506de64de78c847
CansizB <138613983+CansizB@users.noreply.github.com>
2024-11-11 14:12:16 +0300

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,4 +1,7 @@
+import os
+
 import numpy as np
+import pickle
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -8,6 +11,7 @@ from sklearn.metrics import accuracy_score, f1_score, balanced_accuracy_score, c
 import ReqFunc
 from ReqFunc import ImgRecon
 from EITExp import ModelTypes
+
 
 
 def listdefinder(prefix):
@@ -49,7 +53,6 @@ def FitWorkflow(CSVname, mfile_path, EITname, model_name="densenet201", nb_class
 
     estimators = create_estimators()
     for fold in range(5):
-        #c= str(i+1)
         
         X_train, X_test, y_train, y_test = ImgRecon().GenSet(fold=fold, CSVname = CSVname, EITname=EITname, mfile_path = mfile_path, nb_classes= nb_classes, save=True)
     
@@ -61,34 +64,78 @@ def FitWorkflow(CSVname, mfile_path, EITname, model_name="densenet201", nb_class
     
     
         for j in range(3):
-    
-            if j == 5:
-                model = ModelTypes().IPWM(model_name= model_name)
-                print("IPWM was utilized")
+
+            if model_name == "hybrid":
+                if j == 0: 
+                    
+                    model1 = ModelTypes().IPWM(model_name= "densenet201")
+                    model2 = ModelTypes().IPWM(model_name= "resnet50")
+                    print("IPWM is currently being utilized.")
+                    
                 
-            
-            elif j == 0:
-                modelId = "FTM"
-                model = ModelTypes().FTM(model_name= model_name, nb_classes= nb_classes)
+                elif j == 1:
+                    modelId = "FTM"
 
-                print("Fine-tuning of FTM is currently being utilized.") 
-                ReqFunc.ftExp(net = model, fold = fold, ExpName = modelId, nb_epoch = 20, nb_classes= 5)
-                model = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
-    
-                print("Feature extraction is being processed by FTM") 
-            
+                    model1 = ModelTypes().FTM(model_name= "densenet201", nb_classes= nb_classes)
+                    model2 = ModelTypes().FTM(model_name= "resnet50", nb_classes= nb_classes)
+                    print("Fine-tuning of FTM is currently being utilized.")
+                    ReqFunc.ftExp(net = model1, fold = fold, ExpName = modelId, nb_epoch = 1, nb_classes= nb_classes)
+                    ReqFunc.ftExp(net = model2, fold = fold, ExpName = modelId, nb_epoch = 1, nb_classes= nb_classes)
+                    model1 = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+                    model2 = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+                    print("Feature extraction is being processed by FTM.") 
+                
+                else:
+                    model1 = ModelTypes().FTADLM(model_name= "densenet201", nb_classes= nb_classes)
+                    model2 = ModelTypes().FTADLM(model_name= "resnet50", nb_classes= nb_classes)
+                    print("Fine-tuning of FTADLM is currently being utilized.") 
+                    ReqFunc.ftExp(net = model1, fold = fold, ExpName = modelId, nb_epoch = 1, nb_classes= nb_classes)
+                    ReqFunc.ftExp(net = model2, fold = fold, ExpName = modelId, nb_epoch = 1, nb_classes= nb_classes)
+                    model1 = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+                    model2 = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+                    print("Feature extraction is being processed by FTADLM.") 
+
+                feature_vectors_train1 = ReqFunc.extract_features(model1, [X_train3d])
+                feature_vectors_test1 = ReqFunc.extract_features(model1, [X_test3d])
+
+                feature_vectors_train2 = ReqFunc.extract_features(model2, [X_train3d])
+                feature_vectors_test2 = ReqFunc.extract_features(model2, [X_test3d])
+
+                feature_vectors_train= np.concatenate([feature_vectors_train1, feature_vectors_train2],axis=1)
+                feature_vectors_test= np.concatenate([feature_vectors_test1, feature_vectors_test2],axis=1)
+
             else:
-                modelId = "FTADLM"
-                model = ModelTypes().FTM(model_name= model_name, nb_classes= nb_classes)
+                if j == 0:
+                    model = ModelTypes().IPWM(model_name= model_name)
+                    print("IPWM is currently being utilized.")
+                    feature_vectors_train = ReqFunc.extract_features(model, [X_train3d])
+                    feature_vectors_test = ReqFunc.extract_features(model, [X_test3d])
+                    
+                
+                elif j == 1:
+                    modelId = "FTM"
+                    model = ModelTypes().FTM(model_name= model_name, nb_classes= nb_classes)
 
-                print("Fine-tuning of FTADLM is currently being utilized.") 
-                ReqFunc.ftExp(net = model, fold = fold, ExpName = modelId, nb_epoch = 10, nb_classes= 5)
-                model = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
-    
-                print("Feature extraction is being processed by FTADLM.") 
-    
-            feature_vectors_train = ReqFunc.extract_features(model, [X_train3d])
-            feature_vectors_test = ReqFunc.extract_features(model, [X_test3d])
+                    print("Fine-tuning of FTM is currently being utilized.") 
+                    ReqFunc.ftExp(net = model, fold = fold, ExpName = modelId, nb_epoch = 20, nb_classes= nb_classes)
+                    model = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+        
+                    print("Feature extraction is being processed by FTM") 
+                    feature_vectors_train = ReqFunc.extract_features(model, [X_train3d])
+                    feature_vectors_test = ReqFunc.extract_features(model, [X_test3d])
+                
+                else:
+                    modelId = "FTADLM"
+                    model = ModelTypes().FTADLM(model_name= model_name, nb_classes= nb_classes)
+
+                    print("Fine-tuning of FTADLM is currently being utilized.") 
+                    ReqFunc.ftExp(net = model, fold = fold, ExpName = modelId, nb_epoch = 10, nb_classes= nb_classes)
+                    model = ReqFunc.modelImp(ExpName = modelId, foldId=fold, nb_classes=nb_classes)
+        
+                    print("Feature extraction is being processed by FTADLM.") 
+                    feature_vectors_train = ReqFunc.extract_features(model, [X_train3d])
+                    feature_vectors_test = ReqFunc.extract_features(model, [X_test3d])
+        
     
             sc = StandardScaler().fit(feature_vectors_train)
             X_trainSS = sc.transform(feature_vectors_train)
@@ -188,3 +235,30 @@ def FitWorkflow(CSVname, mfile_path, EITname, model_name="densenet201", nb_class
             proba_modelHGBC.append(HGBC.predict_proba(X_testSS)) 
             proba_modelSoVC.append(SoVC.predict_proba(X_testSS))
             proba_modelSC.append(SC.predict_proba(X_testSS))
+    
+    ALL_ACC_LIST=[AccList_SVM_LIN,AccList_SVM_RBF, AccList_HGBC, AccList_RFC, AccList_SC, AccList_SoVC, AccList_HVC]
+    ALL_bAcc_LIST=[bAccList_SVM_LIN,bAccList_SVM_RBF, bAccList_HGBC, bAccList_RFC, bAccList_SC, bAccList_SoVC, bAccList_HVC]
+    ALL_F1_LIST=[F1List_SVM_LIN,F1List_SVM_RBF, F1List_HGBC, F1List_RFC, F1List_SC, F1List_SoVC, F1List_HVC]
+
+    Results= [ALL_ACC_LIST, ALL_bAcc_LIST, ALL_F1_LIST]
+    Results= np.array(Results)
+
+
+    if not os.path.exists("Results"):
+        os.makedirs("Results")
+
+    np.save(f"Results/C{nb_classes}Results", Results)
+
+
+    with open("Results/proba_modelRBF.txt", "wb") as fp:   
+        pickle.dump(proba_modelRBF, fp)
+    with open("Results/proba_modelRFC.txt", "wb") as fp:  
+        pickle.dump(proba_modelRFC, fp)
+    with open("Results/proba_modelSC.txt", "wb") as fp:   
+        pickle.dump(proba_modelSC, fp)
+    with open("Results/proba_modelLIN.txt", "wb") as fp:  
+        pickle.dump(proba_modelLIN, fp)
+    with open("Results/proba_modelHGBC.txt", "wb") as fp:  
+        pickle.dump(proba_modelHGBC, fp)
+    with open("Results/proba_modelSoVC.txt", "wb") as fp:   
+        pickle.dump(proba_modelSoVC, fp)
